9d3d5c9a245340f3259f04080094078d3a154f96
Sokngim Sa <sokngimsa@gmail.com>
2018-06-27 16:21:25 +0700

    Update

--- app/src/main/java/com/example/macbookpro/srrufriend/MainActivity.java
+++ app/src/main/java/com/example/macbookpro/srrufriend/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.macbookpro.srrufriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.macbookpro.srrufriend.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+        //Add fragment to Activity
+        if(savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+    }//Main Method
+}// Main Class
